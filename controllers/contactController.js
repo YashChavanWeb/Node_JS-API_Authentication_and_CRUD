a07e5f4477b343eedecf97cbbf8533a7060ec313
Yash Chavan <yashchavan4628@gmail.com>
2024-07-11 12:25:56 +0530

    Add files via upload

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,18 @@
 // @route GET /api/contacts
 // @access public
 const getContacts = (req, res) => {
-    res.status(200).json({ msg: 'Get all contacts' })
+    // res.status(200).json({ msg: 'Get all contacts' })
+    // console.log(req.body)
+    // res.json({message: req.body})
+
+
+    // destructuring and error handling when we get an empty object
+    const {name, email, phone} = req.body;
+    res.send(req.body)
+    if(!name || !email || !phone) {
+        res.status(400)
+            throw new Error("Please enter all the fields")
+        }
 }
 
 
